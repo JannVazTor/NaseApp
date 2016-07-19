1eef95a2e3299f08c904c6c337ffe586d54918e7
Leonardo Durazo <leo.durazod@gmail.com>
2016-07-18 19:42:54 -0700

    Reporte Productores.

--- app/scripts/services/reportService.js
+++ app/scripts/services/reportService.js
@@ -2,7 +2,7 @@
     'use strict'
     angular.module('naseNutAppApp').factory('reportService',function($http, apiPath){
         var _getProducerReport = function(id){
-            return $http.get(apiPath + 'api/report/producer/' + id);
+            return $http.get(apiPath + 'api/producer/' + id);
         }
         var _getSacksPerVariety = function(){
             return $http.get(apiPath + 'api/report/');
